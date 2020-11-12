73ae527589c679077053ee9bc35dfdce9e096067
adospinoiu <32246342+adospinoiu@users.noreply.github.com>
2020-11-11 20:49:38 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "$n@k3t0wn!$myH0m",
+    password: "",
     database: "burger_db"
   });
 };
